1e908b6ff44e6ccb5ad843e4ac756760ab05b00f
David Cortes <carvanasar@gmail.com>
2020-09-18 23:50:55 +0200

    Agregado  funcionalidad de favoritos

--- src/store/index.js
+++ src/store/index.js
@@ -7,14 +7,38 @@ export default new Vuex.Store({
   state: {
     voces: [],
   },
+  getters: {
+    getVoces(state) {
+      return state.voces;
+    },
+    getFavoritos(state) {
+      return state.voces.filter((voz) => voz.favorito);
+    },
+  },
   mutations: {
     setVoces(state, voces) {
       state.voces = voces;
+    },
+    setFavoritos(state, index) {
+      state.voces[index].favorito = true;
+    },
+    unsetFavoritos(state, nombre) {
+      state.voces.map((voz) => {
+        if (voz.id === nombre) {
+          voz.favorito = false;
+        }
+      });
     },
   },
   actions: {
     getDatos({ commit }, voces) {
       commit("setVoces", voces);
+    },
+    favAction({ commit }, index) {
+      commit("setFavoritos", index);
+    },
+    unfavAction({ commit }, index) {
+      commit("unsetFavoritos", index);
     },
   },
 });
